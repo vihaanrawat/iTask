a324a88d931904eda78531461f5d5c46f77a5755
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-10 19:11:00 +0530

    initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const App = () => {
   return (
-    <>
     
-    </>
+    <div className="min-h-screen bg-[#f5f3ff]">
+      <div className='w-full max-w-5xl min-h-screen mx-auto bg-[#111318] p-6 flex flex-col'>
+        
+        <h1 className='text-4xl font-bold text-white text-center'>
+          iTask
+
+        </h1>
+        <p className='text-gray-400 text-center mt-2'>
+          Your Task Planner
+
+        </p>
+      </div>
+
+    </div>
+    
   )
 }
 
