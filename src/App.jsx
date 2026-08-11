f5fc72314c0ac39639483ede2a2fd23180e9a3b6
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-11 13:31:12 +0000

    Aug 11, 2026, 7:01 PM

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ const App = () => {
         </div>
         <div className='mt-6'>
           {tasks.map((item, index) => (
+            
             <p key={index} className='bg-[#f8e7c7] border border-[#c49a6c] rounded-xl px-4 py-4 mb-3 text-[#5a321d]'>
               {item}
             </p>
