d08553202111f389b2e422027f0a16a40af93b37
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-11 13:32:54 +0000

    Aug 11, 2026, 7:02 PM

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,8 @@ const App = () => {
         <div className='mt-6'>
           {tasks.map((item, index) => (
             
-            <p key={index} className='bg-[#f8e7c7] border border-[#c49a6c] rounded-xl px-4 py-4 mb-3 text-[#5a321d]'>
+            <p key={index} className='bg-[#f8e7c7] border border-[#c49a6c] rounded-xl px-4 py-4 mb-3 text-[#5a321d] flex items-center gap-3'>
+              <input type="checkbox" />
               {item}
             </p>
           ))}
