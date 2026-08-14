1fbf2a26a39dea575a82f67fcde281ea9fcc706c
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-14 14:18:06 +0000

    Aug 14, 2026, 7:48 PM

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ const App = () => {
 
 
         <div className='mt-6'>
-          {tasks.filter((item)=>{
+          {tasks.map((item,index)=>({item,index})).filter((item)=>{
             if(filter==='active') return !item.completed
             if(filter==='completed') return item.completed
             return true
